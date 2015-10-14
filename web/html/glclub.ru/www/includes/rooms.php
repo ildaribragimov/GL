4c3690e9248ac0f9e8b5c3cc61d6244018a00e6b
Ildar Ibragimov <iibragimov84@gmail.com>
2015-10-14 15:54:17 +0300

    Завершение верстки секции ОТЗЫВЫ. Начало верстки секции НОМЕРА

---
+++ web/html/glclub.ru/www/includes/rooms.php
@@ -0,0 +1,41 @@
+<?php 
+/**
+ * Описание шаблона вывода секции "Добавить отзыв"
+ */
+?>
+
+<section id="rooms">
+	<div class="section-wrapper">
+		<!-- Содержимое секции - Начало -->
+		<div class="section-content">
+			<div class="roomsItems">
+				<div class="roomsItems-wrapper">
+					<div class="roomsItem -newYork">
+						<div class="room-preview">
+							<div class="room-previewImg"></div>
+							<a href="/index.php?room_number=1">
+								<span class="room-number">
+									<span class="character">#</span>
+									<span class="number">1</span>
+								</span>
+							</a>
+						</div>
+					</div>
+					<div class="roomsItem -japan">
+						<div class="room-preview">
+							<div class="room-previewImg"></div>
+							<a href="/index.php?room_number=2">
+								<span class="room-number">
+									<span class="character">#</span>
+									<span class="number">2</span>
+								</span>
+							</a>
+						</div>
+					</div>
+					<div class="clr"></div>
+				</div>				
+			</div>
+		</div>
+		<!-- Содержимое секции - Конец -->
+	</div>
+</section>
