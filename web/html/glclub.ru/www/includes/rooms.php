7947b580567454cfd342e25c481e2031e9589e34
Ildar Ibragimov <iibragimov84@gmail.com>
2015-10-15 21:35:33 +0300

    Исправление позиционирования панелей НОМЕРОВ, добавление теней и эффектов наведения.

--- web/html/glclub.ru/www/includes/rooms.php
+++ web/html/glclub.ru/www/includes/rooms.php
@@ -13,25 +13,26 @@
 					<div class="roomsItem -newYork">
 						<a href="/index.php?room_number=1">
 							<span class="room-number">
-								<span class="number-wrapper">
-									<span class="character">#</span>
-									<span class="number">1</span>
-								</span>
+								<span class="number-wrapper">#1</span>
 							</span>
 						</a>
 					</div>
 					<div class="roomsItem -japan">
 						<a href="/index.php?room_number=2">
 							<span class="room-number">
-								<span class="number-wrapper">
-									<span class="character">#</span>
-									<span class="number">2</span>
-								</span>
+								<span class="number-wrapper">#2</span>
 							</span>
 						</a>
 					</div>
 					<div class="clr"></div>
-				</div>				
+				</div>
+			</div>
+			<div class="sticker">
+				<div class="sticker-wrapper">
+					<div class="progressBar">
+						<div class="logo -big"></div>
+					</div>
+				</div>
 			</div>
 		</div>
 		<!-- Содержимое секции - Конец -->
