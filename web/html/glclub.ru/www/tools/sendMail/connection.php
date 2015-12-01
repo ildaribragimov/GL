4b39d3ee7309f7a6e8ce607de28be78e8b571e91
Ildar Ibragimov <iibragimov84@gmail.com>
2015-12-01 21:21:33 +0300

    Завершение разработки блока кода проверки E-mail пользователя на существование

--- web/html/glclub.ru/www/tools/sendMail/connection.php
+++ web/html/glclub.ru/www/tools/sendMail/connection.php
@@ -1,60 +1,116 @@
 <?php
-//
-$timeout = 10;
-// Назначние порта
-$port = 25;
-// Получение имени хоста (например glclub.ru)
-$localhost = $_SERVER['HTTP_HOST'];
-// Указание адреса отправителя тестового сообщения на указанный пользователем e-mail для проверки его существования
-$sender = 'info@' . $localhost;
-// Статус проверки по умолчанию
-$result = false;
-// ?
-$id = 0;
+/*
+*  This script was writed by Setec Astronomy - setec@freemail.it
+*
+*  This script is distributed under the GPL License
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*   GNU General Public License for more details.
+*
+*  http://www.gnu.org/licenses/gpl.txt
+*
+*/
+define ('DEBUG_OK', true);
 
-$host = $localhost;
+class CCheckMail {
+	
+	var $timeout = 10;
 
-
-if (getmxrr ($host, $mxhosts[0], $mxhosts[1]) == true)  array_multisort ($mxhosts[1], $mxhosts[0]);
-    else { $mxhosts[0] = $host;
-       $mxhosts[1] = 10;
-     }
-    if (DEBUG_OK) print_r ($mxhosts);
-
-if ($connection = fsockopen ($mxhosts[0][$id], $port, $errno, $error, $this->timeout)) {
-	fputs ($connection,"HELO $localhost\r\n"); // 250
-	$data = fgets ($connection,1024);
-	$response = substr ($data,0,1);
-	if (DEBUG_OK) print_r ($data);
-	// 200, 250 etc.
-	if ($response == '2') {
-		fputs ($connection,"MAIL FROM:<$sender>\r\n");
-		$data = fgets($connection,1024);
-		$response = substr ($data,0,1);
-		if (DEBUG_OK) print_r ($data);
-		// 200, 250 etc.
-		if ($response == '2') {
-			fputs ($connection,"RCPT TO:<$email>\r\n");
-			$data = fgets($connection,1024);
-			$response = substr ($data,0,1);
-			if (DEBUG_OK) print_r ($data);
-			// 200, 250 etc.
-			if ($response == '2') {
-				fputs ($connection,"data\r\n");
-				$data = fgets($connection,1024);
-				$response = substr ($data,0,1);
-				if (DEBUG_OK) print_r ($data);
-				// 200, 250 etc.
-				if ($response == '2') {
-					$result = true;
-				}
-			}
+	function execute ($email = "") {
+		
+		$host = substr (strstr ($email, '@'), 1);
+		
+		if ( getmxrr ($host, $mxhosts[0], $mxhosts[1]) == true ) {
+			array_multisort ($mxhosts[1], $mxhosts[0]);
+		} else {
+			$mxhosts[0] = $host;
+			$mxhosts[1] = 10;
 		}
-	}
-	fputs ($connection,"QUIT\r\n");
-    fclose ($connection);
-	if ($result) return true;
-}
-$id++;
+		
+		if ( DEBUG_OK ) {
+			print_r ($mxhosts);
+		}
 
+		$port = 25;
+		$localhost = $_SERVER['HTTP_HOST'];
+		$sender = 'info@' . $localhost;
+
+		$result = false;
+		$id = 0;
+		
+		while (!$result && $id < count ($mxhosts[0])) {
+			if ( function_exists ("fsockopen") ) {
+				if ( DEBUG_OK ) {
+					print_r ($id . " " . $mxhosts[0][$id]);
+				}
+		 
+				if ( $connection = fsockopen ($mxhosts[0][$id], $port, $errno, $error, $this->timeout) ) {
+					fputs ($connection,"HELO $localhost\r\n"); // 250
+					$data = fgets ($connection,1024);
+					$response = substr ($data,0,1);
+					if ( DEBUG_OK ) {
+						print_r ($data);
+					}
+					// 200, 250 etc.
+					if ($response == '2') {
+						fputs ($connection,"MAIL FROM:<$sender>\r\n");
+						$data = fgets($connection,1024);
+						$response = substr ($data,0,1);
+						if ( DEBUG_OK ) {
+							print_r ($data);
+						}
+						// 200, 250 etc.
+						if ($response == '2') {
+							fputs ($connection,"RCPT TO:<$email>\r\n");
+							$data = fgets($connection,1024);
+							$response = substr ($data,0,1);
+							if ( DEBUG_OK ) {
+								print_r ($data);
+							}
+
+							// 200, 250 etc.
+							if ($response == '2') {
+								fputs ($connection,"data\r\n");
+								$data = fgets($connection,1024);
+								$response = substr ($data,0,1);
+								if ( DEBUG_OK ) {
+									print_r ($data);
+								}
+								// 200, 250 etc.
+								if ( $response == '2' ) {
+									$result = true;
+								}
+							}
+						}
+					}
+					fputs ($connection,"QUIT\r\n");
+					fclose ($connection);
+					if ( $result ) {
+						return true;
+					}
+				}
+			} else {
+				break;
+			}
+			$id++;
+		}
+		
+		return false;
+	}
+
+}
+
+
+
+
+header('Content-Type: text/html; charset=utf-8');
+
+$str='iibragimov844@gmail.com';
+$alter=new CCheckMail ();
+
+echo '<pre>';
+print "E-mail: ".$str." - ".($alter->execute($str)?'существует':'не существует');
+echo '</pre>';
 ?>
