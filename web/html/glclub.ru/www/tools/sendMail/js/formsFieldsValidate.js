9f3d609a5ee35b42a70b11b3dd7eec57a726d334
Ildar Ibragimov <iibragimov84@gmail.com>
2015-12-19 18:40:43 +0300

    - Назначние одного обработчика события "отправка" на все формы - Создание функции "Проверка содержимого элементов формы, отправляемой на сервер"

--- web/html/glclub.ru/www/tools/sendMail/js/formsFieldsValidate.js
+++ web/html/glclub.ru/www/tools/sendMail/js/formsFieldsValidate.js
@@ -4,6 +4,13 @@
 // Исполнение скрипта при готовности DOM-структуры документа
 $.documentReady(function () {
 
+	/* ==== Решение "preventDefault.js"========================== *
+	 * ==== Отмена действия по умолчанию браузера на событие ==== *
+	 * ========================================================== */
+	function preventDefault(event) { event = event || window.event; event.preventDefault ? event.preventDefault() : event.returnValue = false; }
+	/* ========================================================== */
+
+	
 	/**
 	 * Функция Проверяет значения поля формы на допустимость и валидность
 	 *
@@ -11,62 +18,71 @@ $.documentReady(function () {
 	 * * true|false (тип: boolean) - Результат проверки. Да - Проверка значений полей формы прошла успешно; Нет - Проверка завершилась безуспешно
 	 */
 	function checkFormData(form) {
-		/**
-		 * Объявление переменных
-		 *
-		 * formElements (тип: array) - Массив ссылок на элементы формы, которые необходимо проверить
-		 * fields (тип: object) - Объект Элементов формы
-		 * fieldValue (тип: string) - Содержимое поля
-		 
-		var formElements = ['input', 'select', 'textarea'],
-			fields = ,
-			fieldValue = ;
-		*/
-		var elems = form.elements;
-		// Вывод количества элементов формы
-		alert (elems.length);
-		
-		for (var i = 0; i < elems.length; i++) {
-			var t = elems[i]
-			alert(t);
+		// Объявление переменных:
+		var elements = form.elements, // Коллекция объектов элементов формы
+			errorMsg = []; // Массив сообщений об ошибках
+			
+		// Обход коллеции объектов в массиве
+		for (var e = 0; e < elements.length; e++) {
+			// Объявление переменных:
+			var name = elements[e].name, // Значение атрибута "name" элемента формы
+				type = elements[e].type, // Значение атрибута "type" элемента формы
+				value = elements[e].value; // Содержимое элемента формы
+
+			// Если тип элемента формы - button/input или captcha
+			if ( type == 'submit' || ~name.toLowerCase().indexOf('captcha') ) {
+				// Прерывание выполнения текущей итерации и переход к следующей итерации
+				continue;
+			}
+			// Если содержимое элемента формы пустое
+			if ( value == '' ) {
+				// Формирование сообщения об ошибке
+				errorMsg[e] = 'Поле не должно быть пустым!';
+				// Прерывание выполнения текущей итерации и переход к следующей итерации
+				continue;
+			}
+			// Проверка содержимого элемента формы на соответствие присвоенному типу поля
+			switch ( name ) {
+				case 'name':
+					alert ('Это Ваше Имя'); 
+					break;
+				case 'email':
+					alert ('Это Ваш Email');
+					break;
+				case 'phone':
+					alert ('Это Ваш Телефон');
+					break;
+				default:
+					alert ('Это значение не изветстного мне поля');
+					break;
+			}
 		}
 		
-
-		// Вывод имени тэга элемента формы
-		
-		//alert (form.elements.userName);
-		alert('тест');
-		alert(form.innerHTML);
-		
+		if ( errorMsg != '') {
+			alert (errorMsg.join(', '));
+		}
 		
 		// Возвращение положительного результата
 		return true;
 	}
-	
-	/*
-	document.getElementsByTagName('form').onsubmit = function(){
-		alert ('тест');
-	};
-	*/
-	
-	function getFormContent(form) {
-		alert(form.innerHTML);
-		// Запрет на переход по ссылке
-		return false;
+
+
+	/**
+	 * Назначение одного обработчика событий отправки формы всем формам на странице
+	 */
+	// Получение коллекции объектов всех форм, присутствующих на странице
+	var forms = document.getElementsByTagName('form');
+	// Обход коллеции объектов в массиве
+	for (var f = 0; f < forms.length; f++) {
+		// Назначение каждому объкту коллекции обработчика события отправки формы на сервер
+		forms[f].addEventListener("submit", function(event) {
+			// Вызов функции предварительной проверки данных перед отправкой на сервер
+			checkFormData(this);
+			// 
+			//turingTest(this);
+			// Отмена действия по умолчанию браузера на событие
+			preventDefault(event);
+		});
 	}
-	
-	
-	// Назначение одного обработчика событий отправки для разных форм
-	document.querySelectorAll('button').addEventListener("submit", getFormContent(this));
-	
-	/*
-	document.forms.sendMail.sendMail
-		.onclick = function(){
-			// Вызов функции проверки значений полей формы
-			checkFormData( document.forms.sendMail );			
-			
-			// Запрет на переход по ссылке
-			return false;
-		};
-		*/
+
 });
