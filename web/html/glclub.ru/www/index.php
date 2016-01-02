81b179586c5b5ac4481da63866d3595062697e86
Ildar Ibragimov <iibragimov84@gmail.com>
2016-01-02 20:58:19 +0300

    Внес изменения в связи с переименованием файла поддержки старых браузеров

--- web/html/glclub.ru/www/index.php
+++ web/html/glclub.ru/www/index.php
@@ -15,7 +15,7 @@
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
         <script defer src="template/js/prototypesExt.js" type="text/javascript"></script>
-        <script defer src="template/js/support-IE9+.js" type="text/javascript"></script>
+        <script defer src="template/js/support.js" type="text/javascript"></script>
 		<link href="template/css/normalize.css" rel="stylesheet" type="text/css">
 		<link href="template/css/fonts.css" rel="stylesheet" type="text/css">
         <link href="template/css/common.css" rel="stylesheet" type="text/css">
