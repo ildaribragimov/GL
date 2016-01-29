fc0c9b29aee8f4216a100d2ad5e73cdb851626ce
Ildar Ibragimov <iibragimov84@gmail.com>
2016-01-29 20:43:08 +0300

    Создание HTML-макета секции "Забронировать номер" без кнопки

---
+++ web/html/glclub.ru/www/includes/reserveRoom.php
@@ -0,0 +1,37 @@
+<?php 
+/**
+ * Описание шаблона вывода секции "Контакты"
+ */
+?>
+
+<section id="reserveRoom">
+	<a name="reserveRoom"></a>
+	<div class="section-wrapper">
+		<!-- Шапка секции - Начало -->
+		<header>
+			<h2>Хотите забронировать номер?</h2>
+            <div class="separator -phone">
+				<div class="separator-wrapper">
+					<div class="separator-content">
+						<hr>
+						<div class="icon -s"></div>
+						<hr>
+					</div>
+				</div>
+				<div class="clr"></div>
+			</div>
+		</header>
+		<!-- Шапка секции - Конец -->
+		
+		<!-- Содержимое секции - Начало -->
+		<div class="section-content">
+			<div class="content-wrapper">
+                <p>Уточнить состояние номеров на желаемое вами дату и время посещения <br>вы можете по телефону!</p>
+                <div class="subsection-phone">
+                    <p><span class="tel">+7 (963) 122 55 08</span></p>
+                </div>
+			</div>
+		</div>
+		<!-- Содержимое секции - Конец -->
+	</div>
+</section>
