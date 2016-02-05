72dcc3435de240f4fded0e518438f5812de030fb
Ildar Ibragimov <iibragimov84@gmail.com>
2016-02-05 21:29:10 +0300

    Продолжение разработки объекта высплывающего окна

--- web/html/glclub.ru/www/template/js/plugin.popupWindow.js
+++ web/html/glclub.ru/www/template/js/plugin.popupWindow.js
@@ -6,26 +6,57 @@
  *
  * Свойства объекта:
  * * name (тип: string) - Имя всплывающего окна. Будет использовано в значении атрибута ID конструкции
- * * elements (тип: Array) - Массив селекторов элементов, над которыми будут проходить манипуляции
- * * enable (тип: Boolean) - Состояние панели навигации (false - свернута; true - развернута)
+ * * type (тип: string) - Тип всплывающего окна. может принимать значения: "alert", "confirm", "prompt"
+ * * options (тип: object) - Объект параметров всплывающего окна. Содержит слудующие параметры:
+ * * * header (тип: string) - Заголовок окна
+ * * * navigation (тип: string) - Тип навигационной панели. Может принимать значения: "standart", "minimal".
+ * * data (тип: object) - Объект передаваемых данных для вывода в окне
  *
  * Методы объекта:
  * * show - Метод разворачивает панель
  * * hide - Метод сворачивает панель
  */
-function popupWindow(name){
-    // Создание нового тега "div"
-    var div = document.createElement('div');
-    // Добавление к элементу атрибута "id"
-    div.setAttribute("id", name);
-    // Добавление к элементу атрибута "class"
-    div.setAttribute("class", "popupWindow");
-    
-    
+function popUp(name, options) {
+    // Назначение значений по умолчанию параметрам параметрам всплывающего окна, если они не были переданы в вызове
+    options = options || new Object;
+    options.type = options.type || "alert";
+    options.header = options.header || null;
+    options.data = options.data || "Данные не были переданы!";
+    options.navigation = options.navigation || "standart";
+    /**
+     * Объявление перменных:
+     *
+     ** htmlTree (тип: object) - HTML-конструкция всплывающего окна
+     ** parentElem (тип: object) - Ссылка на родильский элемент текущего создаваемого элемента
+     */
+    var htmlTree = ["popupWindow", "wrapper", "content"],
+        parentElem = null,
+        popupWindow = null;
+
+    //
+    for (var t = 0; t < htmlTree.length; t++) {
+        // Создание нового тега "div"
+        var elem = document.createElement('div');
+        // Если текущий элемент - первый
+        if ( t == 0 ) {
+            // Добавление к элементу атрибута "id"
+            elem.setAttribute("id", name);   
+        }
+        // Добавление к элементу атрибута "class"
+        elem.setAttribute("class", "popupWindow");
+        // Если создаваемый эелемент имеет родителя
+        if ( parentElem ) {
+            // Вставляем текущий элемент в конец родительского
+            parentElem.appendChild(elem);
+        }
+        // Назначаем текущий элемент в качестве родительс кого для следующего элемента
+        parentElem = elem;
+    }
     
     console.log('Создан объект '+name);
-    
 }
 
 // Создание экземпляра объекта "Всплывающее окно" с ID 
-var googleMap = new popupWindow('googleMap');
+var googleMap = new popUp("googleMap", {navigation:"minimal"});
+
+alert(googleMap.innerHTML );
